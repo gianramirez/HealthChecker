02e4552dfb5a5552df2d823906cf3cf2b38988fc
IamDevSteph <stephanie.ingraham3493@gmail.com>
2023-01-23 22:57:07 -0500

    Add existing project files to Git

---
+++ src/test/java/com/alumni/HealthChecker/HealthCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alumni.HealthChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
